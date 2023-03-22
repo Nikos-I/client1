ad13e38790cf1f9e5a61a8b3a9be20a061f13364
Nikos-I <109852894+Nikos-I@users.noreply.github.com>
2023-03-22 16:41:39 +0300

    Finish commit

---
+++ chat_client.py
@@ -0,0 +1,30 @@
+# Чат клиент
+import socket
+import select
+import sys
+import threading
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def sock_rec():
+    while True:
+        data_in = server.recv(2048)    
+        print (data_in.decode())
+        print('\n')
+        
+
+IP_address = "192.168.1.101"
+Port = 65432
+
+server.connect((IP_address, Port))
+
+rec_thread = threading.Thread(target=sock_rec)
+rec_thread.start()
+
+while True:
+    message = input("Введите сообщение: ").encode()
+    server.send(message)
+    print('<Вы> ')
+    print(message.decode())
+    # sys.stdout.flush()
+server.close()
